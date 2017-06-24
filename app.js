bac74e8132b195234b645a9a4f0ac30a4737e486
Giuseppe Bertucci <bertuccig@gmail.com>
2017-06-24 12:38:29 +0100

    amended controller logic

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 (function () {
 'use strict';
 
+    var MAX_MEALS = 3;
+    var EMPTY_MESSAGE = "";
+    var RIGHT_MESSAGE = "Enjoy!";
+    var TOO_MANY_MESSAGE = "Too much!";
+    
     angular
         .module('LunchCheck', [])
         .controller('LunchCheckController', function ($scope) {
 
-            $scope.total = 0;
+            $scope.message = EMPTY_MESSAGE;
+            $scope.meals = "";
             $scope.calc = function() {
               
-                $scope.total = $scope.pippo.split(',').length;
+                $scope.message = getMessage();
             };
+        
+            function getMessage() {
+              
+                var total = $scope.meals.split(',').length;
+
+//                if(total == 0) return EMPTY_MESSAGE;
+                
+                if(total > MAX_MEALS) return TOO_MANY_MESSAGE;
+                else return RIGHT_MESSAGE;
+            };
+            
         });
+
     
 })();
